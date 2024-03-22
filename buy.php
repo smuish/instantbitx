50847507ab899c3ac9c2f789bca2a2fc635eb718
smuish <kasc4u@gmail.com>
2024-03-22 15:37:49 +0000

    Updates on buy/sell/home
    updated the buy and sell process

--- buy.php
+++ buy.php
@@ -1,12 +1,223 @@
-<div class="col-md-12">
+<div class="col-lg-12 col-xl-12 col-sm-12 col-md-12">
     <div class="card">
         <div class="header">
             <h3 class="title">Buy Crypto</h3>
-            <p class="category">Are you looking for more components? Please check our Premium Version of Paper Dashboard Pro.</p>
+            <p class="category"><small>Get the best rates for your currency</small></p>
         </div>
         <div class="content">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<div>
+ <div class="row">
+  <div class="process">
+   <div class="process-row nav nav-tabs">
+    <div class="process-step">
+     <button type="button" class="btn btn-info btn-circle" data-toggle="tab" href="#menu1"><i class="fa fa-info fa-2x"></i></button>
+     <p><small>Buy<br />Amount</small></p>
+    </div>
+    <div class="process-step">
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu2"><i class="fa fa-file-text-o fa-2x"></i></button>
+     <p><small>Payment<br />Method</small></p>
+    </div>
+    <div class="process-step">
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu3"><i class="fa fa-ticket fa-2x"></i></button>
+     <p><small>Transaction<br />Summary</small></p>
+    </div>
+    <div class="process-step">
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu4"><i class="fa fa-cogs fa-2x"></i></button>
+     <p><small>Processing<br />Transaction</small></p>
+    </div>
+    <div class="process-step">
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu5"><i class="fa fa-check fa-2x"></i></button>
+     <p><small>Completed<br /></small></p>
+    </div>
+   </div>
+  </div>
+  <div class="tab-content">
+   <div id="menu1" class="tab-pane fade active in">
+    <div class="col-lg-4 col-md-6 col-sm-12">
+    <h3>How much do you want to buy?</h3>
+    <small>1 USD = 12.8 GHC</small>
+    <p>
+    <div class="form-group">
+                <select class="form-control">
+                    <option> Select Coin </option>
+                    <option>Bitcoin</option>
+                    <option>Usdt</option>
+                </select>
+            </div> 
+        
+            <div class="form-group">
+                <input type="text" class="form-control" name="usdamount" id="usdamount" placeholder="Amount in USD" />
+            </div>
+            <div class="form-group">
+                <small style="margin:0 auto">You will pay</small>
+                <input type="text" class="form-control" name="ghcamount" id="ghcamount" placeholder="Amount in GHC" />
+            </div>
+            <div>
+            <small style="margin:0 auto">Your wallet address</small>
+            <input type="text" class="form-control" name="cwallet" id="cwallet" placeholder="bc1q36ef8cf43ga0lwfspm68hle6ed9fytyqnz9j3t" />
+            </div>
 
+            
+            <p><small style="margin:0 auto">Priority Fees:</small></p>
+            <div style="display:flex; justify-content:space-between;">
+            <label class="buysell"><input type="radio" name="pf" id="pf" value="0" /> <span class="checkmark"></span>&nbsp;&nbsp;High</label>
+            <label class="buysell"><input type="radio" name="pf" id="pf" value="1" /> <span class="checkmark"></span>&nbsp;&nbsp;Medium</label>
+            <label class="buysell"><input type="radio" name="pf" id="pf" value="3" /> <span class="checkmark"></span>&nbsp;&nbsp;Low</label>
+            </div>
+        </p>
+    </div>
+    <div class="col-lg-12">
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
+    </ul>
+    </div>
+   </div>
+   <div id="menu2" class="tab-pane fade">
+   <div class="col-lg-8 col-md-6 col-sm-12">
+    <h3>Make Payment</h3>
+    <p>Select how you want to pay</p>
+    <p>
+       <label class="buysell"><input type="radio" name="pm" id="pm" checked value="mbm"> <span class="checkmark"></span>&nbsp;&nbsp;Mobile money </label>
+       <label class="buysell"> <input type="radio" name="pm" id="pm" value="bt"> <span class="checkmark"></span>&nbsp;&nbsp;Bank transfer </label>
+       <label class="buysell"> <input type="radio" name="pm" id="pm" value="cs"> <span class="checkmark"></span>&nbsp;&nbsp;Cash </label>
+    </p>
+    <div class="row" id="mbm">
+    <div class="col-lg-8 col-xl-12 col-md-4 col-sm-4">
+        <label class="mbbuy">
+           <div class="checkma"> <input type="radio" name="mmoney" id="mmoney" value="MTN" /> </div>
+            <div>
+                <div> MTN </div>
+                
+                <div>Mobile Money Name:</div>
+                <div>Mobile Money Number:</div>
+        </div>
+</label>
+        <label class="mbbuy">
+            <div class="checkma"><input type="radio" name="mmoney" id="mmoney" value="Airtel Tigo" /> </div>
+            <div>
+                <div> Airtel Tigo </div>
+                
+                <div>Mobile Money Name:</div>
+                <div>Mobile Money Number:</div>
+            </div>
+</label>
+        <label class="mbbuy">
+            <div class="checkma"> <input type="radio" name="mmoney" id="mmoney" value="Telecel" /> </div>
+            <div>
+                
+            <div> Telecel </div>
+                
+                <div>Mobile Money Name:</div>
+                <div>Mobile Money Number:</div>
+            
+            </div>
+</label>
+    </div>
+    </div>
+
+    <div class="row" id="bt" style="display:none">
+    <div class="col-lg-8 col-xl-12 col-md-4 col-sm-4">
+        <div class="form-group">
+            <input class="form-control" type="text" name="bankn" id="bankn" placeholder="Bank Name" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="aname" id="aname" placeholder="Account Name" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="accountn" id="accountn" placeholder="Account Number" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="bankb" id="bankb" placeholder="Bank Branch" />
+        </div>
+    </div>
+    </div>
+    
+    
+    </div>
+    <div class="col-lg-12">
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
+     <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
+    </ul>
+</div>
+   </div>
+   <div id="menu3" class="tab-pane fade" style="text-align:center">
+    <div class="col-lg-12">
+    <h3>Transaction Summary</h3>
+    <div>
+        <p>Buy Order.</p>
+        <p>Payment Type:</p>
+        <p>Payment Details:</p>
+        <div><oblique>payment details here</oblique></div>
+        <p>Coin Type: </p>
+        <p>Buy Amount (USD): </p>
+        <p>Buy Amount (GHS): </p>
+        <p>Wallet Address:</p>
+        <p>Priority Fees:</p>
+        <p><input type="button" class="btn btn-warning" value="Place buy order" /></p>
+        
+    </div> 
+    </div>
+    <div class="col-lg-12">
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
+     <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
+    </ul>
+    </div>
+   </div>
+   <div id="menu4" class="tab-pane fade" style="text-align:center">
+    <h3>Processing</h3>
+    <p>
+        <div>Please wait while we process your transaction </div>
+        <i class="fa fa-cog fa-spin" style="font-size:30px;color:#d4d4d4"></i>
+    </p>
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
+     <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
+    </ul>
+   </div>
+   <div id="menu5" class="tab-pane fade" style="text-align:center">
+    <h3>Completed</h3>
+    <p>
+        <i class="fa fa-check fa-3x"></i>
+        <div>Transaction confirmed. Processing now </div>
+    </p>
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
+     <li><button type="button" class="btn btn-success"><i class="fa fa-check"></i> Done!</button></li>
+    </ul>
+   </div>
+  </div>
+ </div>
+</div>
         </div>
     </div>
 </div>
+
+<script>
+
+   pm = document.querySelectorAll('#pm')
+   
+   for(i = 0; i < pm.length; i++){
+    pm[i].addEventListener('change', togglePayment)
+   }
+   function togglePayment(){
+    if(this.value == "mbm"){
+
+        document.getElementById(this.value).style.display = "block"
+        document.getElementById("bt").style.display = "none"
+    }else if(this.value == "bt"){
+        document.getElementById(this.value).style.display = "block"
+        document.getElementById("mbm").style.display = "none"
+    }else{
+        document.getElementById("mbm").style.display = "none"
+        document.getElementById("bt").style.display = "none"
+
+    }
+   }
+</script>
+
+
+
                 
--- sell.php
+++ sell.php
@@ -1,16 +1,3 @@
-<style>
-
-.process-step .btn:focus{outline:none}
-.process{display:table;width:100%;position:relative}
-.process-row{display:table-row}
-.process-step button[disabled]{opacity:1 !important;filter: alpha(opacity=100) !important}
-.process-row:before{top:30px;bottom:0;position:absolute;content:" ";width:100%;height:1px;background-color:#ccc;z-order:0}
-.process-step{display:table-cell;text-align:center;position:relative}
-.process-step p{margin-top:4px}
-.btn-circle{width:60px;height:60px;text-align:center;font-size:12px;background-color:#ffffff;}
-
-</style>
-
 <div class="col-lg-12 col-xl-12 col-sm-12 col-md-12">
     <div class="card">
         <div class="header">
@@ -28,35 +15,45 @@
      <p><small>Sell<br />Amount</small></p>
     </div>
     <div class="process-step">
-     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu2"><i class="fa fa-file-text-o fa-2x"></i></button>
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu2"><i class="fa fa-credit-card fa-2x"></i></button>
      <p><small>Payment<br />Method</small></p>
     </div>
     <div class="process-step">
-     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu3"><i class="fa fa-image fa-2x"></i></button>
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu3"><i class="fa fa-ticket fa-2x"></i></button>
      <p><small>Transaction<br />Summary</small></p>
     </div>
     <div class="process-step">
-     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu4"><i class="fa fa-cogs fa-2x"></i></button>
-     <p><small>Configure<br />display</small></p>
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu4"><i class="fa fa-hourglass fa-2x"></i></button>
+     <p><small>Awaiting<br />Assets</small></p>
     </div>
     <div class="process-step">
      <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu5"><i class="fa fa-check fa-2x"></i></button>
-     <p><small>Save<br />result</small></p>
+     <p><small>Confirmation</small></p>
+    </div>
+    <div class="process-step">
+     <button type="button" class="btn btn-default btn-circle" data-toggle="tab" href="#menu6"><i class="fa fa-cogs fa-2x"></i></button>
+     <p><small>Processing</small></p>
     </div>
    </div>
   </div>
   <div class="tab-content">
    <div id="menu1" class="tab-pane fade active in">
-    <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="col-lg-8 col-md-6 col-sm-12">
     <h3>How much do you want to sell?</h3>
     <small>1 USD = 12.8 GHC</small>
     <p>
-        
+            <div class="form-group">
+                <select class="form-control">
+                    <option> Select Coin </option>
+                    <option>Bitcoin</option>
+                    <option>Usdt</option>
+                </select>
+            </div> 
             <div class="form-group">
                 <input type="text" class="form-control" name="usdamount" id="usdamount" placeholder="Amount in USD" />
             </div>
             <div class="form-group">
-                <small style="margin:0 auto">You will receive</small>
+                <span style="position:relative; margin:0 auto">You will receive</span>
                 <input type="text" class="form-control" name="ghcamount" id="ghcamount" placeholder="Amount in GHC" />
             </div>
         </p>
@@ -68,14 +65,52 @@
     </div>
    </div>
    <div id="menu2" class="tab-pane fade">
-   <div class="col-lg-4 col-md-6 col-sm-12">
+   <div class="col-lg-8 col-md-6 col-sm-12">
     <h3>Select Payment Method</h3>
-    <small>Select how you want to receive payment</small>
+    <p>Select how you want to receive payment</p>
     <p>
-       <label class=""> <input type="radio" name="pm" id="pm" style="opacity:0; position:absolute"> Mobile money</label>
-       <label  class=""> <input type="radio" name="pm" id="cpm" style="opacity:0; position:absolute">Cash Wallet
-    </label>
+       <label class="buysell"><input type="radio" name="pm" id="pm" checked value="mbm"> <span class="checkmark"></span>&nbsp;&nbsp;Mobile money </label>
+       <label class="buysell"> <input type="radio" name="pm" id="pm" value="bt"> <span class="checkmark"></span>&nbsp;&nbsp;Bank transfer </label>
+       <label class="buysell"> <input type="radio" name="pm" id="pm" value="cs"> <span class="checkmark"></span>&nbsp;&nbsp;Cash </label>
     </p>
+    <div class="row" id="mbm">
+    <div class="col-lg-8 col-xl-12 col-md-4 col-sm-4">
+        <div class="form-group">
+            <input class="form-control" type="text" name="mname" id="mname" placeholder="Mobile money name" />
+        </div>
+        <div class="form-group">
+            <select name="mnetwork" id="mnetwork" class="form-control">
+                <option>Select phone network</option>
+                <option>Telecel</option>
+                <option>MTN</option> 
+                <option>Airtel Tigo</option>  
+            </select>
+            
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="mnumber" id="mnumber" placeholder="Mobile money number" />
+        </div>
+    </div>
+    </div>
+
+    <div class="row" id="bt" style="display:none">
+    <div class="col-lg-8 col-xl-12 col-md-4 col-sm-4">
+        <div class="form-group">
+            <input class="form-control" type="text" name="bankn" id="bankn" placeholder="Bank Name" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="aname" id="aname" placeholder="Account Name" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="accountn" id="accountn" placeholder="Account Number" />
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="bankb" id="bankb" placeholder="Bank Branch" />
+        </div>
+    </div>
+    </div>
+    
+    
     </div>
     <div class="col-lg-12">
     <ul class="list-unstyled list-inline pull-right">
@@ -84,25 +119,60 @@
     </ul>
 </div>
    </div>
-   <div id="menu3" class="tab-pane fade">
-    <h3>Menu 3</h3>
-    <p>Some content in menu 3.</p>
+   <div id="menu3" class="tab-pane fade" style="text-align:center">
+    <div class="col-lg-12">
+    <h3>Transaction Summary</h3>
+    <div>
+        <p>Sell Order.</p>
+        <p>Payment Type:</p>
+        <p>Payment Details:</p>
+        <div><oblique>payment details here</oblique></div>
+        <p>Coin Type: </p>
+        <p>Selling Amount (USD): </p>
+        <p>Selling Amount (GHS): </p>
+        <p><input type="button" class="btn btn-warning" value="Place sell order" /></p>
+        
+    </div> 
+    </div>
+    <div class="col-lg-12">
+    <ul class="list-unstyled list-inline pull-right">
+     <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
+     <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
+    </ul>
+    </div>
+   </div>
+   <div id="menu4" class="tab-pane fade" style="text-align:center">
+    <h3>Awaiting Asset</h3>
+    <p><i class="fa fa-hourglass fa-7x fa-spin" style="font-size:30px;color:#d4d4d4"></i></p>
+    <div>
+        <p>Go to your wallet and send exactly 0.00 USD to the Bitcoin USD address</p>
+        <div style="border:1px solid #eeeeee; background-color:#fcfcfc; padding:10px 30px; max-width:500px; overflow-x: scroll; margin:0 auto; border-radius:10px">
+            <div style="color:#999999; text-align:left">Instantbitx Bitcoin address:</div>
+            <div style="padding: 10px 0px;"><p>bc1q36ef8cf43ga0lwfspm68hle6ed9fytyqnz9j3t</p></div>
+        </div> 
+    </div>
     <ul class="list-unstyled list-inline pull-right">
      <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
      <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
     </ul>
    </div>
-   <div id="menu4" class="tab-pane fade">
-    <h3>Menu 4</h3>
-    <p>Some content in menu 4.</p>
+   <div id="menu5" class="tab-pane fade" style="text-align:center">
+    <h3>Confirmation</h3>
+    <p>
+        <i class="fa fa-check fa-3x"></i>
+        <div>Transaction confirmed. Processing now </div>
+    </p>
     <ul class="list-unstyled list-inline pull-right">
      <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
      <li><button type="button" class="btn btn-info next-step">Next <i class="fa fa-chevron-right"></i></button></li>
     </ul>
    </div>
-   <div id="menu5" class="tab-pane fade">
-    <h3>Menu 5</h3>
-    <p>Some content in menu 5.</p>
+   <div id="menu6" class="tab-pane fade" style="text-align:center">
+    <h3>Processing</h3>
+    <p>
+        <div>Please wait while we process your transaction </div>
+        <i class="fa fa-cog fa-spin" style="font-size:30px;color:#d4d4d4"></i>
+    </p>
     <ul class="list-unstyled list-inline pull-right">
      <li><button type="button" class="btn btn-default prev-step"><i class="fa fa-chevron-left"></i> Back</button></li>
      <li><button type="button" class="btn btn-success"><i class="fa fa-check"></i> Done!</button></li>
@@ -114,7 +184,28 @@
         </div>
     </div>
 </div>
+<script>
 
+   pm = document.querySelectorAll('#pm')
+   
+   for(i = 0; i < pm.length; i++){
+    pm[i].addEventListener('change', togglePayment)
+   }
+   function togglePayment(){
+    if(this.value == "mbm"){
+
+        document.getElementById(this.value).style.display = "block"
+        document.getElementById("bt").style.display = "none"
+    }else if(this.value == "bt"){
+        document.getElementById(this.value).style.display = "block"
+        document.getElementById("mbm").style.display = "none"
+    }else{
+        document.getElementById("mbm").style.display = "none"
+        document.getElementById("bt").style.display = "none"
+
+    }
+   }
+</script>
 
 
                 
