b05bfd4b13405ddb516f3ec2be86fc3490685167
jessejay4u <164058031+jessejay4u@users.noreply.github.com>
2024-07-15 17:41:55 +0000

    Edit Daily Sales Record
    Forms

---
+++ form.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <!-- Site Title  -->
+    <title>Element Form | ICO Crypto - ICO Landing Page &amp; Multi-Purpose Cryptocurrency HTML Template</title>
+    <!-- Bundle and Base CSS -->
+    <link rel="stylesheet" href="assets/css/vendor.bundle.css?ver=210">
+    <link rel="stylesheet" href="assets/css/style.css?ver=210">
+    <!-- Extra CSS -->
+    <link rel="stylesheet" href="assets/css/theme.css?ver=210">
+</head>
+
+<body class="nk-body body-wider bg-light-alt">
+    <div class="nk-wrap">
+        <header class="nk-header page-header is-transparent is-sticky is-shrink" id="header">
+            <!-- Header @s -->
+            
+            <!-- Banner @s -->
+            <!-- .header-banner @e -->
+        </header>
+        <main class="nk-pages">
+            <section class="section bg-white" style="padding-top: 60px; padding-bottom: 60px;">
+                <div class="container">
+                    <div>
+                        <h3 class="title title-md">Edit Daily Sale Record</h3>
+                        <form action="#" class="form-validate">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label class="field-label">ORDERNO.</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4" style="">
+                                    <label class="field-label">BuyUSDT</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">BUY MORN 1</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label class="field-label">BUY AFT 2</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label class="field-label">BUY EVE 3</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+
+                        <h3 class="title title-md" style="padding-top: 30px">Edit Daily Sale Record</h3>
+                        <form action="#" class="form-validate SELL">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label class="field-label">ORDERNO.</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4" style="">
+                                    <label class="field-label">SELL USDT</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">SELL MORN 1</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label class="field-label">SELL AFT 2</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label class="field-label">SELL EVE 3</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section bg-white" style="padding-top: 60px; padding-bottom: 60px;">
+                <div class="container">
+                    <div style="padding-top: 40px;">
+                        <h3 class="title title-md">SATOSHIE VALUES</h3>
+                        <form action="#" class="form-validate">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label class="field-label">BUY MORN1 SATO</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4" style="">
+                                    <label class="field-label">BUY AFT 2 SATO.</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">BUY EVE 3 SATO</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="section bg-white" style="padding-top: 60px; padding-bottom: 60px; background-color: #f8f9fa;">
+                <div class="container">
+                    <div style="padding-top: 40px;">
+                        <h3 class="title title-md">PAYMENTS & TRANSACTIONS</h3>
+                        <form action="#" class="form-validate">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label class="field-label">TRANSID</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4" style="">
+                                    <label class="field-label">MM1</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">MM2</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">ATCASH</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">TELECEL CASH</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">CASH</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">ABSA BANK</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">GT BANK</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">ECOBANK</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">EXPENSE</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label class="field-label">SAFEIN</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <label class="field-label">SAFEOUT</label>
+                                    <div class="field-wrap">
+                                        <input name="inputname1" type="text" class="input-bordered" id="input-text-2" required>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+
+        </main>
+        <!-- Footer -->
+    </div>
+    <!-- Preloader -->
+    <div class="preloader"><span class="spinner spinner-round"></span></div>
+    <!-- JavaScript -->
+    <script src="assets/js/jquery.bundle.js?ver=210"></script>
+    <script src="assets/js/scripts.js?ver=210"></script>
+    <script src="assets/js/charts.js?ver=210"></script>
+    <script src="assets/js/charts.js?ver=210"></script>
+</body>
+
+</html>
